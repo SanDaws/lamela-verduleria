35c4bcd9e12c29ac353f9f3882585717d15d9c3d
LeandroVR1 <yeifry.121@gmail.com>
2024-07-04 13:15:15 -0500

    fix: menu function creation

--- Program.cs
+++ Program.cs
@@ -2,10 +2,36 @@
 
 class Program
 {
+    static void Menu(){
+        Operations.Title("tienda Lamela");
+        Console.Write(@"
+        opciones:
+        1:Agregar nuevo producto
+        2:Buscar producto
+        3:Editar producto
+        4:Eliminar producto
+        s:Salir
+        ");
+        try
+        {
+        char option= Convert.ToChar(Console.ReadLine());
+        }
+        catch (System.FormatException)
+        {
+            Console.WriteLine("Respuesta vacia");
+            Menu();
+        }
+
+
+    }
 
     static void Main(string[] args)    {
-        operations.Menu("tienda Lamela");
+        Menu();
+
+
+
     }
+
     
 }
 
