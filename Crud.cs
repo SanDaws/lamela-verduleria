5498fe81814fa6ad81aebaf4e1d3d73e62ca2ba4
sanDaws <castrogiraldosantiago@gmail.com>
2024-07-05 09:40:02 -0500

    fix:  console cleaner and titles apllied

--- Crud.cs
+++ Crud.cs
@@ -4,13 +4,24 @@ namespace lamela_verduleria
     
     class Crud
     {
-        public static void Create(Dictionary<string,string[]> product){
+        public static void Create(Dictionary<string,int[]> product){
+            Operations.ConsoClean();
+            Operations.Title("Agregar nuevo producto");
         }
-        public static void Read(Dictionary<string,string[]> product){}
+        public static void Read(Dictionary<string,int[]> product){
+            Operations.ConsoClean();
+            Operations.Title("Buscar producto");
+        }
 
-        public static void Update(Dictionary<string,string[]> product){}
+        public static void Update(Dictionary<string,int[]> product){
+            Operations.ConsoClean();
+            Operations.Title("Editar producto");
+        }
 
-        public static void Delete(Dictionary<string,string[]> product){}
+        public static void Delete(Dictionary<string,int[]> product){
+            Operations.ConsoClean();
+            Operations.Title("Eliminar producto");
+        }
 
 
 
