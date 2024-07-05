2abfc254740f87bfdd2cd39653446c713f06a8d1
sanDaws <castrogiraldosantiago@gmail.com>
2024-07-05 09:35:55 -0500

    fix:crud remnamed

---
+++ Crud.cs
@@ -0,0 +1,18 @@
+namespace lamela_verduleria
+{
+    
+    
+    class Crud
+    {
+        public static void Create(Dictionary<string,string[]> product){
+        }
+        public static void Read(Dictionary<string,string[]> product){}
+
+        public static void Update(Dictionary<string,string[]> product){}
+
+        public static void Delete(Dictionary<string,string[]> product){}
+
+
+
+    }
+}
